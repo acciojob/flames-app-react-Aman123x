19090ea79248546a88afd8c6a3490917c5971a6e
Aman Kumar <96618245+Aman123x@users.noreply.github.com>
2023-12-20 17:05:06 +0000

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,58 @@
-import React, {Component, useState} from "react";
+import React, { useState} from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    render() {
+let arr=["Siblings","Friends","Love","Affection","Marriage","Enemy",];
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
+const App=()=> {
+
+    const [name1,setName1]=useState("");
+    const [name2,setName2]=useState("");
+    const [relationship,setRelationship]=useState("");
+
+    function calculateRelationship(e){
+        e.preventDefault();
+        let str1=name1;
+        let str2=name2;
+
+        for(let t of str1){
+            if(str2.includes(t)){
+                str1=str1.replace(t,"");
+                str2=str2.replace(t,"");
+            }
+        }
+        setName1(str1);
+        setName2(str2);
+
+        setRelationship(arr[(str1.length+str2.length)%6]);
     }
+
+
+    return(
+        <div id="main">
+            <form>
+                <input type="text" data-testid="input1" 
+                  onChange={(e)=>setName1(e.target.value)}
+                  value={name1}
+                 />
+                <input type="text" data-testid="input2" onChange={(e)=>setName2(e.target.value)}/>
+                <button data-testid="calculate_relationship" type="submit" 
+                value={name2}
+                    onClick={calculateRelationship}
+                >Calculate Relationship Future</button>
+                <button data-testid="clear" type="reset"
+                    onClick={
+                        ()=>{
+                            setName1="";
+                            setName2="";
+                        }
+                    }
+                >Clear</button>
+            </form>
+
+            <h3 data-testid="answer">{relationship}</h3>
+
+        </div>
+    )
 }
 
 
